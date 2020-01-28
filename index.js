3f9613b7e2e93b754a2d65ce9c693d2f0ff4debf
Repo manager <rugiguru@gmail.com>
2020-01-28 17:56:00 +0530

    mongo db tutorials

---
+++ index.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/playground", { useNewUrlParser: true,useUnifiedTopology: true })
+.then(res => console.log('Connected to db'));
+
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags:[ String ],
+    date: {type : Date, default: Date.now},
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name:'React js course',
+        author: 'Guru Prasad',
+        tags:['React', 'frontend'],
+        isPublished: true
+    })
+    const result = await course.save();
+    console.log(result)
+}
+
+async function getCourses () {
+    const courses = await Course
+        .find({ author:'Guru Prasad', isPublished:true})
+        .limit(10)
+        .sort({name:1})
+        .select({name:1, tags:1})
+
+    console.log(courses)
+}
+
+getCourses();
+
+
